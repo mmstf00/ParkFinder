d70811503b0507441a4f08f6db2e14d81bc4308a
Mustafa <mustafa.ercan392@gmail.com>
2023-01-09 00:37:08 +0200

    Refactor duplicated JS functions

--- src/main/resources/static/js/indexPageMapLoader.js
+++ src/main/resources/static/js/indexPageMapLoader.js
@@ -14,24 +14,24 @@ function initMap() {
     // Allows to scroll without pressing CTRL button.
     map.setOptions({scrollwheel: true});
 
-    for (let marker of markers) {
-        addMarker(map, marker);
-    }
+    initMarkers(map);
 }
 
-function addMarker(map, marker) {
-    let advancedMarker = new google.maps.marker.AdvancedMarkerView({
-        map,
-        content: setMarkerPrice(marker.price),
-        position: marker.position,
-    });
+function initMarkers(map) {
+    for (let marker of markers) {
+        let advancedMarker = new google.maps.marker.AdvancedMarkerView({
+            map,
+            content: setMarkerPrice(marker.price),
+            position: marker.position,
+        });
 
-    // Adding zoom event to the marker.
-    advancedMarker.addListener('click', function () {
-        // Pan to the marker's position and set the zoom level
-        map.panTo(advancedMarker.position);
-        map.setZoom(14); // TODO: Calculate Map zoom and marker zoom for smooth zoom result.
-    });
+        // Adding zoom event to the marker.
+        advancedMarker.addListener('click', function () {
+            // Pan to the marker's position and set the zoom level
+            map.panTo(advancedMarker.position);
+            map.setZoom(14); // TODO: Calculate Map zoom and marker zoom for smooth zoom result.
+        });
+    }
 }
 
 function setMarkerPrice(price) {
