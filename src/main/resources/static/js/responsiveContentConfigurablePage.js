56385055176dca722d5d65eb8f5bb32ff4630b06
Mustafa <mustafa.ercan392@gmail.com>
2023-01-13 01:08:58 +0200

    Fix bigger map size and add styling to scrollbar

--- src/main/resources/static/js/responsiveContentConfigurablePage.js
+++ src/main/resources/static/js/responsiveContentConfigurablePage.js
@@ -6,8 +6,19 @@ let headerHeight = header.offsetHeight;
 map.style.top = headerHeight + "px";
 markerAdderContainer.style.top = headerHeight - 60 + "px";
 
+// Fixing map and park list size to be exactly fullscreen
+let viewportHeight = window.innerHeight;
+let valueInPixels = (100 * viewportHeight) / 100 - headerHeight;
+map.style.height = valueInPixels + "px";
+markerAdderContainer.style.height = valueInPixels + "px";
+
 window.onresize = function () {
     let headerHeight = header.offsetHeight;
     map.style.top = headerHeight + "px";
     markerAdderContainer.style.top = headerHeight - 60 + "px";
+
+    viewportHeight = window.innerHeight;
+    valueInPixels = (100 * viewportHeight) / 100 - headerHeight;
+    map.style.height = valueInPixels + "px";
+    markerAdderContainer.style.height = valueInPixels + "px";
 };
--- src/main/resources/static/js/responsiveContentMainPage.js
+++ src/main/resources/static/js/responsiveContentMainPage.js
@@ -6,8 +6,19 @@ let headerHeight = header.offsetHeight;
 map.style.top = headerHeight + "px";
 parksList.style.top = headerHeight - 60 + "px";
 
+// Fixing map and park list size to be exactly fullscreen
+let viewportHeight = window.innerHeight;
+let valueInPixels = (100 * viewportHeight) / 100 - headerHeight;
+map.style.height = valueInPixels + "px";
+parksList.style.height = valueInPixels + "px";
+
 window.onresize = function () {
-    let headerHeight = header.offsetHeight;
+    headerHeight = header.offsetHeight;
     map.style.top = headerHeight + "px";
     parksList.style.top = headerHeight - 60 + "px";
+
+    viewportHeight = window.innerHeight;
+    valueInPixels = (100 * viewportHeight) / 100 - headerHeight;
+    map.style.height = valueInPixels + "px";
+    parksList.style.height = valueInPixels + "px";
 };
