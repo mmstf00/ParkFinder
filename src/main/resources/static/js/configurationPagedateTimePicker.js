5d67fb37775730a20c5561160de686331acf8a31
Mustafa <mustafa.ercan392@gmail.com>
2023-01-22 11:46:02 +0200

    Add validation to date/time pickers
    * Users cannot pick date before start date

--- src/main/resources/static/js/configurationPagedateTimePicker.js
+++ src/main/resources/static/js/configurationPagedateTimePicker.js
@@ -1,9 +1,21 @@
-config = {
+let configDateFromPicker = flatpickr("#config-date-from", {
     enableTime: true,
     minDate: "today",
     altInput: true,
-    altFormat: "Y M d , H:i",
+    altFormat: "M d at H:i",
     dateFormat: "Y-m-d H:i:S",
-}
+    onChange: function (selectedDates, dateStr, instance) {
+        let newTime = new Date(selectedDates[0].getTime() + 1800000);
+        let minTime = new Date(selectedDates[0].getTime() + 300000);
+        configDateToPicker.setDate(newTime, true);
+        configDateToPicker.set("minDate", minTime);
+    }
+});
 
-flatpickr("input[type=datetime-local]", config);
+let configDateToPicker = flatpickr("#config-date-to", {
+    enableTime: true,
+    minDate: "today",
+    altInput: true,
+    altFormat: "M d at H:i",
+    dateFormat: "Y-m-d H:i:S"
+});
--- src/main/resources/static/js/dateTimePicker.js
+++ src/main/resources/static/js/dateTimePicker.js
@@ -1,9 +1,21 @@
-config = {
+let dateFromPicker = flatpickr("#from-date-picker", {
+    enableTime: true,
+    minDate: "today",
+    altInput: true,
+    altFormat: "M d at H:i",
+    dateFormat: "Y-m-d H:i:S",
+    onChange: function (selectedDates, dateStr, instance) {
+        let newTime = new Date(selectedDates[0].getTime() + 1800000);
+        let minTime = new Date(selectedDates[0].getTime() + 300000);
+        dateToPicker.setDate(newTime, true);
+        dateToPicker.set("minDate", minTime);
+    }
+});
+
+let dateToPicker = flatpickr("#to-date-picker", {
     enableTime: true,
     minDate: "today",
     altInput: true,
     altFormat: "M d at H:i",
     dateFormat: "Y-m-d H:i:S"
-}
-
-flatpickr("input[type=datetime-local]", config);
+});
