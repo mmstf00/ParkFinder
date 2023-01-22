0510c618aed093d51d6734dd49bef26a9dbf34ed
Mustafa <mustafa.ercan392@gmail.com>
2023-01-22 23:44:22 +0200

    Add design to park list items

--- src/main/java/com/parkfinder/model/MarkerDTO.java
+++ src/main/java/com/parkfinder/model/MarkerDTO.java
@@ -1,6 +1,7 @@
 package com.parkfinder.model;
 
 import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 import org.springframework.format.annotation.DateTimeFormat;
@@ -9,6 +10,7 @@ import java.time.LocalDateTime;
 
 @Data
 public class MarkerDTO {
+    @Max(value = 40, message = "Address must be with max 40 length")
     private String address;
     @DecimalMin(value = "0.1", message = "Price must be greater than 0")
     private double priceTag;
