df959f1e7484fa957783255a724e56f3d20997dc
Mustafa <mustafa.ercan@gmail.com>
2023-04-12 22:34:15 +0300

    Enhance input validation

--- src/main/java/com/parkfinder/model/MarkerDTO.java
+++ src/main/java/com/parkfinder/model/MarkerDTO.java
@@ -8,7 +8,10 @@ import org.hibernate.validator.constraints.Length;
 @Getter
 @Setter
 public class MarkerDTO {
-    @Length(min = 10, max = 40, message = "Address must be with max 40 length")
+    @Length.List({
+        @Length(min = 10, message = "Address must be at least 10 characters"),
+        @Length(max = 40, message = "Address must be less than 40 characters")
+    })
     private String address;
     private String placeId;
     @DecimalMin(value = "0.1", message = "Price must be greater than 0")
