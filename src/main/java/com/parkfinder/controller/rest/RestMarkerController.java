b7882cd109fa622ee7c8d8eb9528af0d1637322f
Mustafa <mustafa.ercan392@gmail.com>
2023-05-16 22:07:13 +0300

    Add Swagger documentation to REST endpoints

--- src/main/java/com/parkfinder/controller/rest/RestMarkerController.java
+++ src/main/java/com/parkfinder/controller/rest/RestMarkerController.java
@@ -4,6 +4,8 @@ import com.parkfinder.entity.Marker;
 import com.parkfinder.model.ReservationRequest;
 import com.parkfinder.model.UpdateRequest;
 import com.parkfinder.service.ExtendedMarkerService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.HttpStatus;
@@ -16,22 +18,26 @@ import java.util.List;
 @RestController
 @RequestMapping("api/v1")
 @RequiredArgsConstructor
+@Tag(name = "Parkfinder REST API")
 public class RestMarkerController {
 
     private final ExtendedMarkerService markerService;
 
+    @Operation(summary = "Returns all park locations")
     @GetMapping(produces = "application/json")
     public ResponseEntity<List<Marker>> getAllMarkers() {
         List<Marker> markers = markerService.getMarkers();
         return new ResponseEntity<>(markers, HttpStatus.OK);
     }
 
+    @Operation(summary = "Returns park location by given id")
     @GetMapping("/{id}")
     public ResponseEntity<Marker> getById(@PathVariable Long id) {
         Marker marker = markerService.getMarkerById(id);
         return new ResponseEntity<>(marker, HttpStatus.OK);
     }
 
+    @Operation(summary = "Returns park location by given latitude and longitude")
     @GetMapping(value = "/getMarker", produces = "application/json")
     public ResponseEntity<Marker> getMarker(@RequestParam("lat") double latitude,
                                             @RequestParam("lng") double longitude) {
@@ -39,6 +45,7 @@ public class RestMarkerController {
         return new ResponseEntity<>(markerByLatLng, HttpStatus.OK);
     }
 
+    @Operation(summary = "Returns all available park locations for given datetime range")
     @GetMapping(value = "/search", produces = "application/json")
     public ResponseEntity<List<Marker>> getAllByDateRange(
             @RequestParam("dateFrom") @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss") LocalDateTime dateFrom,
@@ -47,18 +54,21 @@ public class RestMarkerController {
         return new ResponseEntity<>(allBySelectedDateRange, HttpStatus.OK);
     }
 
+    @Operation(summary = "Makes reservation on a park location")
     @PutMapping(consumes = "application/json", produces = "application/json")
     public ResponseEntity<String> makeReservation(@RequestBody ReservationRequest reservationRequest) {
         markerService.makeReservation(reservationRequest);
         return new ResponseEntity<>(HttpStatus.OK);
     }
 
+    @Operation(summary = "Updates park location data")
     @PutMapping(value = "/update")
     public ResponseEntity<String> updateMarker(@RequestBody UpdateRequest updateRequest) {
         markerService.updateMarker(updateRequest);
         return new ResponseEntity<>(HttpStatus.OK);
     }
 
+    @Operation(summary = "Deletes park location by given id")
     @DeleteMapping(value = "/{id}")
     public ResponseEntity<String> deleteMarker(@PathVariable Long id) {
         markerService.deleteMarkerById(id);
